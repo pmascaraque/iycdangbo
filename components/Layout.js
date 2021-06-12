3e81fd0b9627ab435a605c138f0c28d0595450ef
pmascaraque <ssm.pablo@gmail.com>
2021-06-13 01:28:16 +0200

    layout

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
