5f24af19f9cc123fdb68dd709250730e3102b823
pmascaraque <ssm.pablo@gmail.com>
2021-06-13 01:33:18 +0200

    footer

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <a href="https://www.facebook.com/Asociaci%C3%B3n-Infancia-y-Cirug%C3%ADa-en-Dangbo-1414041928901454">facebook</a>
+        </div>
+    )
+}
