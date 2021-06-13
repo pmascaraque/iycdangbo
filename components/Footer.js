4ce83d70ffda72947664043817b0499dba2ff99a
pmascaraque <ssm.pablo@gmail.com>
2021-06-13 02:13:54 +0200

    imported image

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <div>
       <a href="https://www.facebook.com/Asociaci%C3%B3n-Infancia-y-Cirug%C3%ADa-en-Dangbo-1414041928901454">facebook</a>
+      <a href="https://www.instagram.com/cirugia_en_dangbo/">instagram</a>
     </div>
   );
 }
--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,13 @@ import React from "react";
 import Link from "next/link";
 
 export default function Header() {
+
   return (
     <div>
       <Link href="/">
-        <a>Home</a>
+        <a>
+          <img alt="" src={"./images/dangbo_logo.jpg"} />
+        </a>
       </Link>
     </div>
   );
