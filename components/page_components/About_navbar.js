97ece4dddab47b0b6e8ab3b3090a65872240cf38
pmascaraque <ssm.pablo@gmail.com>
2021-06-15 00:34:13 +0200

    about navbar styling

--- components/page_components/About_navbar.js
+++ components/page_components/About_navbar.js
@@ -5,15 +5,9 @@ import styles from "../component_styles/About.module.css";
 export default function About_navbar() {
   return (
     <div className={styles.header}>
-      <Link href="/">
-        <a>DANGBO</a>
-      </Link>
-      <Link href="/about">
-        <a>OUR WORK</a>
-      </Link>
-      <Link href="/news">
-        <a>HOW TO HELP</a>
-      </Link>
+      <button>DANGBO</button>
+      <button>OUR WORK</button>
+      <button>HOW YOU CAN HELP</button>
     </div>
   );
 }
