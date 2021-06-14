93642627a97419bb285bfbeec8f5e930145ca97b
pmascaraque <ssm.pablo@gmail.com>
2021-06-15 00:44:40 +0200

    active navbar hook

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
-import About_navbar from "../components/page_components/About_navbar.js";
+import styles from "../components/component_styles/About.module.css";
 
 function About() {
+  const [active, setActive] = useState(1);
   return (
     <Layout>
-      <About_navbar />
-      ABOUT
+      <div className={styles.header}>
+        <button
+          onClick={() => {
+            setActive(1);
+          }}
+        >
+          DANGBO
+        </button>
+        <button
+          onClick={() => {
+            setActive(2);
+          }}
+        >
+          OUR WORK
+        </button>
+        <button
+          onClick={() => {
+            setActive(3);
+          }}
+        >
+          HOW YOU CAN HELP
+        </button>
+      </div>
+      {active}
     </Layout>
   );
 }
