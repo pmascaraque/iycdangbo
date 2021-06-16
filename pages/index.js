d2dd7900f070c2ba1e541ab9f40eb1235a3c6aa9
pmascaraque <ssm.pablo@gmail.com>
2021-06-16 06:52:09 +0200

    navbar styling

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { render } from "react-dom";
 import Layout from "../components/Layout";
 import styles from "../components/component_styles/Index.module.css";
+import { Link, Element, Events, animateScroll as scroll, scrollSpy, scroller } from "react-scroll";
+import { Dangbo, Work, Help } from "../components/page_components/about_texts";
 
 function Index() {
   return (
     <Layout>
-      <a className={styles.active} href="https://infanciaycirugiaendangbo.org/">https://infanciaycirugiaendangbo.org/</a>
+      <div className={styles.navbar}>
+        <Link activeClass={styles.active} to="test1" spy={true} smooth={true} duration={500}>
+          Test 1
+        </Link>
+        <Link activeClass={styles.active} to="test2" spy={true} smooth={true} duration={500}>
+          Test 2
+        </Link>
+        <Link activeClass={styles.active} to="test3" spy={true} smooth={true} duration={500}>
+          Test 3
+        </Link>
+      </div>
+      <div id="test1">
+        <Dangbo />
+      </div>
+      <div id="test2">
+        <Work />
+      </div>
+      <div id="test3">
+        <Help />
+      </div>
     </Layout>
   );
 }
