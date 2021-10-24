baff5957412ee300eea66b6b855c93aebc5993a4
pmascaraque <ssm.pablo@gmail.com>
2021-10-24 13:24:21 +0200

    donate func

--- pages/donate.js
+++ pages/donate.js
@@ -10,13 +10,8 @@ const stripePromise = loadStripe(
 function Donate() {
   return (
     <Layout>
-      <br /> <br /> <p>a</p>
-      <br />
       <div className="bg-white my-4 shadow p-8 rounded-lg">
-        <div className="flex items-center mb-4">
-          <div className="border-2 border-blue px-3 py-2 rounded-full font-bold text-blue mr-2">1</div>
-          <h2 className="text-lg">Your Payment Information</h2>
-        </div>
+        <h2 className="text-lg">Your Payment Information</h2>
 
         <div className="w-full">
           <label className="block text-sm mb-2">Credit Card</label>
@@ -24,29 +19,61 @@ function Donate() {
             <input
               type="text"
               id="payment"
-              class="w-5/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
-              placeholder="Card Number"
+              class="w-1/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+              placeholder="Full Name"
             />
             <input
               type="text"
               id="payment"
               class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
-              placeholder="MM / YY"
+              placeholder="email"
             />
             <input
               type="text"
               id="payment"
               class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
+              placeholder="Address"
+            />
+                        <input
+              type="text"
+              id="payment"
+              class="w-1/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+              placeholder="City"
+            />
+            <input
+              type="text"
+              id="payment"
+              class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
+              placeholder="State"
+            />
+            <input
+              type="text"
+              id="payment"
+              class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
+              placeholder="ZIP Code"
+            />
+          </div>
+          <div className="flex">
+            <input
+              type="text"
+              id="payment"
+              class="w-2/4 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+              placeholder="Card Number"
+            />
+            <input
+              type="text"
+              id="payment"
+              class="w-1/4 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
+              placeholder="MM / YY"
+            />
+            <input
+              type="text"
+              id="payment"
+              class="w-1/4 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
               placeholder="CVC"
             />
           </div>
         </div>
-        <input
-          type="text"
-          id="payment"
-          class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
-          placeholder="Quantity €"
-        />
       </div>
       <Elements stripe={stripePromise}></Elements>
       <form action="/create-checkout-session" method="POST">
