a24eb0e80df6f7ddbe751c2e1c1cb8a2f7652528
pmascaraque <ssm.pablo@gmail.com>
2021-10-30 00:31:04 +0200

    tough luck with cant resolve fs

--- pages/donate.js
+++ pages/donate.js
@@ -3,99 +3,116 @@ import Layout from "../components/Layout";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements, CardElement } from "@stripe/react-stripe-js";
 
+const express = require("express");
+const app = express();
 const stripePromise = loadStripe(
-  "sk_test_51JnN3zHnwRpJy9ynJMJUpGBoT8fiyAQQFR3qKPos6NwAWZrGrRDAecr0KLNRLD6LTP47GrlkdIGVZjRYOOBJ9ixO00kCzNLlVp"
-); // TEST KEY
+  "pk_test_51JnN3zHnwRpJy9ynon9s3tID7EGhAlZzukRevAvodhXUbQTokppHJEUCOllMdzFw1o8c3044fDzUBmmlVb1tQPcb00VJ0tig1T"
+);
 
+app.get("/secret", async (req, res) => {
+  const intent = res.json({ client_secret: intent.client_secret }); // ... Fetch or create the PaymentIntent
+});
 
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
+
+(async () => {
+  const response = await fetch("/secret");
+  const { client_secret: clientSecret } = await response.json();
+  // Render the Payment Element using the clientSecret
+})();
 
 function Donate() {
   const AmountRef = useRef();
   return (
     <Layout>
-      <div className="bg-white my-4 shadow p-8 rounded-lg">
-        <h2 className="text-lg">Your Payment Information</h2>
+      <div>
+        <Elements stripe={stripePromise} options={options}>
+          <div className="bg-white my-4 shadow p-8 rounded-lg">
+            <h2 className="text-lg">Your Payment Information</h2>
 
-        <div className="w-full">
-          <label className="block text-sm mb-2">Credit Card</label>
-          <div className="flex">
-            <input
-              type="text"
-              id="payment"
-              class="w-1/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
-              placeholder="Full Name"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
-              placeholder="email"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
-              placeholder="Address"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
-              placeholder="City"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
-              placeholder="State"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
-              placeholder="ZIP Code"
-            />
+            <div className="w-full">
+              <label className="block text-sm mb-2">Credit Card</label>
+              <div className="flex">
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+                  placeholder="Full Name"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
+                  placeholder="email"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
+                  placeholder="Address"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/6 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+                  placeholder="City"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
+                  placeholder="State"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/6 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
+                  placeholder="ZIP Code"
+                />
+              </div>
+              <div className="flex">
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/5 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+                  placeholder="Amount"
+                  ref={AmountRef}
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-2/5 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
+                  placeholder="Card Number"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/5 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
+                  placeholder="MM / YY"
+                />
+                <input
+                  type="text"
+                  id="payment"
+                  class="w-1/5 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
+                  placeholder="CVC"
+                />
+              </div>
+            </div>
+            <form action="/create-checkout-session" method="POST">
+              <button
+                type="submit"
+                onClick={() => {
+                  console.log(AmountRef.current.value * 100);
+                }}
+              >
+                Submit
+              </button>
+            </form>
           </div>
-          <div className="flex">
-            <input
-              type="text"
-              id="payment"
-              class="w-1/5 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
-              placeholder="Amount"
-              ref={AmountRef}
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-2/5 flex-1 text-sm bg-grey-light text-grey-darkest rounded-l p-3 focus:outline-none"
-              placeholder="Card Number"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/5 inline-block text-sm bg-grey-light text-grey-darkest p-3 focus:outline-none"
-              placeholder="MM / YY"
-            />
-            <input
-              type="text"
-              id="payment"
-              class="w-1/5 inline-block text-sm bg-grey-light text-grey-darkest rounded-r p-3 focus:outline-none"
-              placeholder="CVC"
-            />
-          </div>
-        </div>
+        </Elements>
       </div>
-      <button
-        onClick={() => {
-          console.log(AmountRef.current.value * 100);
-        }}
-      >
-        LOG
-      </button>
-      <Elements stripe={stripePromise}></Elements>
-      <form action="/create-checkout-session" method="POST">
-        <button type="submit">Donate</button>
-      </form>
     </Layout>
   );
 }
