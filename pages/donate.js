d83eb50a011da6ffdd90ec1bcf37594865a32247
pmascaraque <ssm.pablo@gmail.com>
2021-11-19 04:35:13 +0100

    donate.js

--- pages/donate.js
+++ pages/donate.js
@@ -7,12 +7,40 @@ import Stripe from "Stripe";
 //price should be *100
 
 import CheckoutForm from "../components/page_components/CheckoutForm";
+const stripePromise = loadStripe(
+  "pk_test_51JnN3zHnwRpJy9ynon9s3tID7EGhAlZzukRevAvodhXUbQTokppHJEUCOllMdzFw1o8c3044fDzUBmmlVb1tQPcb00VJ0tig1T"
+);
 
 function Donate() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] })
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe"
+  };
+  const options = {
+    clientSecret,
+    appearance
+  };
+
   return (
-    <Layout>
-      <CheckoutForm />
-    </Layout>
+    <div>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
   );
 }
 
