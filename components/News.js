10f612cda4bbd4fa8c89e59013a4f91143076d24
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-19 13:25:32 +0100

    Adding News sections into Index

---
+++ components/News.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Link from "next/link";
+import data from "../data/news";
+
+export default function News({ lastPosts }) {
+  let postList = [];
+  lastPosts.results.map((element) => {
+    let publicationDate = new Date(element.first_publication_date);
+    console.log("Element: ", JSON.stringify(element));
+    postList.push(
+      <div
+        className="bg-white p-4 mt-2 mx-4 rounded-xl md:mr-2 md:w-10/12 lg:w-5/12 xl:w-3/12 border border-opacity-10 border-double border-black"
+        key={element.id}
+      >
+        <h1 className="text-xl mb-2">{element.data.title[0].text}</h1>
+        <p className="mb-8 text-xs text-gray-800">{Intl.DateTimeFormat("de-DE").format(publicationDate)}</p>
+        <p className="mb-5">{element.data.summary[0].text}</p>
+        <Link
+          href={{
+            pathname: "news/[id]",
+            query: {
+              id: element.slugs[0]
+            }
+          }}
+        >
+          <a className="bg-black hover:bg-blue-800 text-white p-2">Leer más</a>
+        </Link>
+      </div>
+    );
+  });
+  return (
+    <div className="border-t-8 border-black pb-10 flex flex-col">
+      <h1 className="pt-10 w-min text-4xl mx-auto">{data.title}</h1>
+      <div className="pt-2 pb-10 md:flex md:justify-center md:flex-wrap md:px-12">{postList}</div>
+      <Link href="/news">
+        <a className="bg-black hover:bg-blue-800 text-white p-2 mx-auto">Más noticias</a>
+      </Link>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
-import { Link } from "react-scroll";
 import { client } from "../config/prismic-configuration";
 import Prismic from "prismic-javascript";
 import Story from "../components/Story";
+import News from "../components/News";
 
-function Index({ posts }) {
+function Index({ lastPosts }) {
   return (
     <Layout>
       <Hero />
       <Story />
+      <News lastPosts={lastPosts} />
     </Layout>
   );
 }
 
 export default Index;
 
-// export const getStaticProps = async () => {
-//   const posts = await client.query(Prismic.Predicates.at("document.type", "post"));
-//   return {
-//     props: {
-//       posts
-//     }
-//   };
-// };
+export const getStaticProps = async () => {
+  const lastPosts = await client.query(Prismic.Predicates.at("document.type", "post"), { pageSize: 3 });
+  console.log(lastPosts);
+  return {
+    props: {
+      lastPosts
+    }
+  };
+};
