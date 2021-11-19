2d9cc56fd2a210cd67add2448b578bef12d49f86
pmascaraque <ssm.pablo@gmail.com>
2021-11-19 04:28:38 +0100

    server.js

--- server/server.js
+++ server/server.js
@@ -5,21 +5,24 @@ const stripe = require("stripe")(
   "sk_test_51JnN3zHnwRpJy9ynJMJUpGBoT8fiyAQQFR3qKPos6NwAWZrGrRDAecr0KLNRLD6LTP47GrlkdIGVZjRYOOBJ9ixO00kCzNLlVp"
 );
 
-const paymentIntent = await stripe.paymentIntents.create({
-  amount: 1099,
-  currency: "eur",
-  payment_method_types: ["card"]
+app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099, //placeholder amount for testing only, must be *100
+    currency: "eur",
+    automatic_payment_methods: {
+      enabled: true
+    }
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`);
-});
-
-app.post("/", (req, res) => {
-  console.log("post sent");
-  res.send("sending post");
-});
+app.listen(4242, () => console.log("Node server listening on port 4242!"));
