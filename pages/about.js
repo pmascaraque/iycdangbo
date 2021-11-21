2f613cdd1471c6ed25b8c0ca59aab9edb4010c17
pmascaraque <ssm.pablo@gmail.com>
2021-11-22 00:12:14 +0100

    new about component files

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../components/Layout";
 
 function About() {
-  const [active, setActive] = useState(1);
+
   return (
     <Layout>
 a
