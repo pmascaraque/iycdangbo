6ee48a36208eb4eafab8c8d299a31b6b8b03150c
pmascaraque <ssm.pablo@gmail.com>
2021-11-21 22:45:37 +0100

    about page wiping

--- pages/about.js
+++ pages/about.js
@@ -1,35 +1,11 @@
 import React, { useState } from "react";
 import Layout from "../components/Layout";
-import { Dangbo, Work, Help } from "../components/page_components/about_texts";
 
 function About() {
   const [active, setActive] = useState(1);
   return (
     <Layout>
-      <div>
-        <button
-          onClick={() => {
-            setActive(1);
-          }}
-        >
-          DANGBO
-        </button>
-        <button
-          onClick={() => {
-            setActive(2);
-          }}
-        >
-          NOSOTROS
-        </button>
-        <button
-          onClick={() => {
-            setActive(3);
-          }}
-        >
-          COMO PUEDES AYUDAR
-        </button>
-      </div>
-      <div>{active === 1 ? <Dangbo /> : active === 2 ? <Work /> : <Help />}</div>
+a
     </Layout>
   );
 }
