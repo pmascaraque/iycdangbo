351ba53564b0ddb558f33b4a11e29a737c02a97e
pmascaraque <ssm.pablo@gmail.com>
2021-11-22 02:30:32 +0100

    rough draft about done

--- components/about/Dangbo.js
+++ components/about/Dangbo.js
@@ -2,11 +2,19 @@ import data from "/data/about";
 
 function Dangbo() {
   return (
-    <div className="h-screen w-full relative">
-      <h1 className="absolute text-white z-10 text-5xl md:text-6xl md:pl-12 lg:text-8xl md:w-3/6 text-left bottom-14 left-4 w-4/6">
-        <span className="font-semibold">{data.boldWord}</span> {data.text}
-      </h1>
-      <img src={data.imageURL} alt={data.imageAlt} className="absolute h-screen object-cover object-center" />
+    <div>
+      <div className="h-screen w-full relative font-medium top-16 border-t-8 border-black">
+        <h1 className="hidden md:inline absolute top-8 right-20 text-6xl">{data.boldWord}</h1>
+        <div className="absolute z-10 tiny:text-base sm:text-xl top-12 md:w-1/2 md:text-2xl md:top-12 lg:leading-normal lg:text-2xl lg:top-28 3xl:text-5xl 2xl:leading-normal">
+          <p className="relative md:left-12 lg:left-24">{data.text1}</p>
+          <p className="relative md:left-12 lg:left-24">{data.text2}</p>
+        </div>
+        <img
+          className="absolute z-10 bottom-12 right-1/4 w-2/4 md:right-12 md:top-64 md:w-2/6 lg:w-4/12 "
+          src={data.locationImageURL}
+        />
+        <img src={data.imageURL} className="h-screen object-cover w-screen object-center" />
+      </div>
     </div>
   );
 }
---
+++ data/about.js
@@ -0,0 +1,7 @@
+export default {
+    boldWord:"Dangbo",
+    text1:"Dangbo es una localidad situada en la Región de Ouemé, al sur de Benín con una población de 77.000 habitantes en una Superficie de 340 kms  cuadrados, con una estructura sanitaria deficitaria e insuficiente.La Republica de Benín es un Estado de África Occidental  situado en el Golfo de Guinea que se extiende de norte a sur en una estrecha franja de 120 kms de ancho entre Togo Y Nigeria y limita con Níger y Burkina Faso por el norte.",
+    text2:"Lo habitan nueve millones de habitantes, franco-parlantes, pertenecientes a más de siete etnias que comparten por igual la belleza del país y la pobreza extrema, que lo coloca en los últimos puestos de desarrollo humano. IDH (índice desarrollo humano): puesto 172. La relación de médicos por habitante es de 0.1 cada 1000 (España 4.9), habiendo 0.2 camas por cada 1000 habitantes (España 3.2). La mortalidad infantil, la desnutrición y las enfermedades infecciosas son la primera causa de mortalidad.",
+    imageURL:"https://ik.imagekit.io/300/0IyCDangboResources/FONDODANGBOOK_xcBh-s1kN.jpg?updatedAt=1637544532269",
+  };
+  
