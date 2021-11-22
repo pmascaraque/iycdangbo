4d82226718a273a001d9713257c7594601e1b3e0
pmascaraque <ssm.pablo@gmail.com>
2021-11-22 06:44:27 +0100

    sponsors page completed! yay!

--- components/about/Sponsors.js
+++ components/about/Sponsors.js
@@ -1,23 +1,22 @@
 import data from "/data/sponsors";
 import React from "react";
 
-console.log(data);
-
-const Tile = ({ sponsorUrl, imageURL }) => {
-  return (
-    <a href={sponsorUrl}>
-      <img src={imageURL} />
-    </a>
-  );
-};
-
-const Tiles = ({ data }) => {
-  const tileList = data.map((tile) => <Tile key={data.link} imageUrl={data.imageURL} />);
-  return <div>{tileList}</div>;
-};
-
 function Sponsors() {
-  return <Tiles data={data} />;
+  let linksList = [];
+  data.forEach((element) => {
+    linksList.push(
+      <a href={element.link} target="_blank" className="inline-block m-4">
+        <img src={element.imageURL} width="120" />
+      </a>
+    );
+  });
+  console.log({ linksList });
+  return (
+    <div className="h-screen w-full relative font-medium top-16 border-t-8 border-black">
+      <h1 className="relative top-8 left-1/3 text-6xl">Solidarios</h1>
+      <div className="relative top-16 grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 justify-items-center">{linksList}</div>
+    </div>
+  );
 }
 
 export default Sponsors;
