b0adf6e6a2df3bd7fccfd5a3478fad1c55949661
pmascaraque <ssm.pablo@gmail.com>
2021-11-22 01:21:21 +0100

    about data file

--- components/about/Dangbo.js
+++ components/about/Dangbo.js
@@ -1,7 +1,14 @@
+import data from "/data/about";
+
 function Dangbo() {
-    return (
-        <div>a</div>
-    );
+  return (
+    <div className="h-screen w-full relative">
+      <h1 className="absolute text-white z-10 text-5xl md:text-6xl md:pl-12 lg:text-8xl md:w-3/6 text-left bottom-14 left-4 w-4/6">
+        <span className="font-semibold">{data.boldWord}</span> {data.text}
+      </h1>
+      <img src={data.imageURL} alt={data.imageAlt} className="absolute h-screen object-cover object-center" />
+    </div>
+  );
 }
 
 export default Dangbo;
