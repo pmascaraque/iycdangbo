eb565d10cf54022b366bd4385d5c4eb1b211842b
pmascaraque <ssm.pablo@gmail.com>
2021-11-22 05:20:48 +0100

    added sponsors object with images and corresponding urls

---
+++ data/sponsors.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    imageURL:
+      "https://ik.imagekit.io/300/0IyCDangboResources/elarcadenoe-265x300_-SPRWusZy.png?updatedAt=1637553254055",
+    link: "https://www.facebook.com/Arca-De-No%C3%A9-Torrelodones-1940902579455884/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/capone-150x150_VeNaKAwDi.jpg?updatedAt=1637553253524",
+    link: "https://restaurante-capone.com/"
+  },
+  {
+    imageURL:
+      "https://ik.imagekit.io/300/0IyCDangboResources/torrempresarial_logo_pUFyNx2DT.jpg?updatedAt=1637553252636",
+    link: "https://www.facebook.com/torrempresarial/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/asistiapc_logo_gp3VrfxdQT.png?updatedAt=1637553253104",
+    link: "https://www.asistia-pc.es/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/Unknown_jVwIYfvEm.jpeg?updatedAt=1637553252792",
+    link: "https://www.torrelodones.es/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/prox_E8Xcgs2CB.jpg?updatedAt=1637553252359",
+    link: "" //NOWEBSITEFOUND
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/koons-studio-_5vHP3NKSS6.png?updatedAt=1637553251659",
+    link: "https://www.koonstudio.com/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/hipicaelalbero_kRpNI8XW8.png?updatedAt=1637553251577",
+    link: "https://www.elalbero.es/main/"
+  },
+  {
+    imageURL:
+      "https://ik.imagekit.io/300/0IyCDangboResources/masquecafe_logo-300x123_b9cTfwXnW.png?updatedAt=1637553252180",
+    link: "http://www.masquecafe.es/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/lapolca-300x180_5g-5o7w_M.png?updatedAt=1637553251968",
+    link: "https://www.facebook.com/jugueteslapolca"
+  },
+  {
+    imageURL:
+      "https://ik.imagekit.io/300/0IyCDangboResources/caballogoloso-300x228_CMXqycoAz.png?updatedAt=1637553253394",
+    link: "https://elcaballogoloso.com/"
+  },
+  {
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/gs_sjCtIaEli.png?updatedAt=1637553251635",
+    link: "https://www.facebook.com/LaGalletaSolidaria/timeline"
+  }
+];
