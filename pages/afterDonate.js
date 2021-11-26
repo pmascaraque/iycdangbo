acaf7687135fbac3f98830312895686349b2ca04
pmascaraque <ssm.pablo@gmail.com>
2021-11-26 22:47:58 +0100

    after donate page

---
+++ pages/afterDonate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "/components/layout/Layout";
+
+function afterDonate() {
+  return (
+    <Layout>
+      CONTACT
+    </Layout>
+  );
+}
+
+export default afterDonate;
