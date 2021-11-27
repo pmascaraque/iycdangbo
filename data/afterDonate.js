1f211ed3d3e3e4e5688d1082f83641bc1a7e57b9
pmascaraque <ssm.pablo@gmail.com>
2021-11-27 11:16:37 +0100

    donate data

---
+++ data/afterDonate.js
@@ -0,0 +1,6 @@
+export default {
+    boldWord: "Dangbo ",
+    text: "necesita tu ayuda",
+    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/BANNER1_RqeUUdexv.jpg?updatedAt=1634987720062"
+  };
+  
--- pages/afterDonate.js
+++ pages/afterDonate.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "/components/layout/Layout";
-import Data from "/data/afterDonate"
+import data from "/data/afterDonate"
 
 function afterDonate() {
   return (
@@ -9,7 +9,7 @@ function afterDonate() {
         <div className="h-screen w-full relative font-medium top-16 border-t-8 border-black">
           <h1 className="hidden md:inline absolute top-8 right-20 text-6xl">{data.boldWord}</h1>
           <div className="absolute z-10 tiny:text-base sm:text-xl top-12 md:w-1/2 md:text-2xl md:top-12 lg:leading-normal lg:text-2xl lg:top-28 3xl:text-5xl 2xl:leading-normal">
-            <p className="relative md:left-12 lg:left-24">{data.text1}</p>
+            <p className="relative md:left-12 lg:left-24">{data.text}</p>
           </div>
           <img src={data.imageURL} className="h-screen object-cover w-screen object-center" />
         </div>
