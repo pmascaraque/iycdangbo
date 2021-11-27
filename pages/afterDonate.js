a0090a3cf5f2bd494544988724f11ff1f8613a92
pmascaraque <ssm.pablo@gmail.com>
2021-11-27 11:05:18 +0100

    boilerplate afterdonate page

--- pages/afterDonate.js
+++ pages/afterDonate.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Layout from "/components/layout/Layout";
+import Data from "/data/afterDonate"
 
 function afterDonate() {
   return (
     <Layout>
-      CONTACT
+      <div>
+        <div className="h-screen w-full relative font-medium top-16 border-t-8 border-black">
+          <h1 className="hidden md:inline absolute top-8 right-20 text-6xl">{data.boldWord}</h1>
+          <div className="absolute z-10 tiny:text-base sm:text-xl top-12 md:w-1/2 md:text-2xl md:top-12 lg:leading-normal lg:text-2xl lg:top-28 3xl:text-5xl 2xl:leading-normal">
+            <p className="relative md:left-12 lg:left-24">{data.text1}</p>
+          </div>
+          <img src={data.imageURL} className="h-screen object-cover w-screen object-center" />
+        </div>
+      </div>
     </Layout>
   );
 }
