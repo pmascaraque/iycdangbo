65cdb276579d1eb390b173e8215ed86b8a6bb3bb
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-11-29 11:17:55 +0100

    Adding thanks page with confetti animation

--- data/afterDonate.js
+++
@@ -1,6 +0,0 @@
-export default {
-    boldWord: "Dangbo ",
-    text: "necesita tu ayuda",
-    imageURL: "https://ik.imagekit.io/300/0IyCDangboResources/BANNER1_RqeUUdexv.jpg?updatedAt=1634987720062"
-  };
-  
