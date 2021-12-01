269defaecbb4df7162d568c2c8b745557fccdef7
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-12-01 15:39:11 +0100

    Improving data/about.js structure

--- components/about/Extra.js
+++ components/about/Extra.js
@@ -1,21 +1,13 @@
 import data from "/data/about";
 
 function Extra() {
-  const mapText = (data) => (
-    <div>
-      {data.map((element) => (
-        <p>{element}</p>
-      ))}
-    </div>
-  );
-
   return (
     <div className="pb-40 w-full relative font-medium top-16">
-      <h1 className="hidden md:inline text-4xl">{data.boldWord4}</h1>
+      <h1 className="hidden md:inline text-4xl"></h1>
       <div className="md:grid grid-cols-3 place-items-center">
-        <div className="">{mapText(data.text5)}</div>
-        <div className="">{mapText(data.text6)}</div>
-        <div className="">{mapText(data.text7)}</div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
       </div>
     </div>
   );
--- data/about.js
+++ data/about.js
@@ -1,29 +1,35 @@
 export default {
-  boldWord1: "Dangbo",
-  boldWord2: "iycDangbo",
-  boldWord3: "Solidarios",
-  boldWord4: "Más Información",
-  text1:
-    "Dangbo es una localidad situada en la Región de Ouemé, al sur de Benín con una población de 77.000 habitantes en una Superficie de 340 kms  cuadrados, con una estructura sanitaria deficitaria e insuficiente.La Republica de Benín es un Estado de África Occidental  situado en el Golfo de Guinea que se extiende de norte a sur en una estrecha franja de 120 kms de ancho entre Togo Y Nigeria y limita con Níger y Burkina Faso por el norte.",
-  text2:
-    "Lo habitan nueve millones de habitantes, franco-parlantes, pertenecientes a más de siete etnias que comparten por igual la belleza del país y la pobreza extrema, que lo coloca en los últimos puestos de desarrollo humano. IDH (índice desarrollo humano): puesto 172. La relación de médicos por habitante es de 0.1 cada 1000 (España 4.9), habiendo 0.2 camas por cada 1000 habitantes (España 3.2). La mortalidad infantil, la desnutrición y las enfermedades infecciosas son la primera causa de mortalidad.",
-  text3:
+  headers: ["Dangbo", "iycDangbo", "Solidarios", "Más Información"],
+  dangbo: [
+    {
+      text: [
+        "Dangbo es una localidad situada en la Región de Ouemé, al sur de Benín con una población de 77.000 habitantes en una Superficie de 340 kms  cuadrados, con una estructura sanitaria deficitaria e insuficiente.La Republica de Benín es un Estado de África Occidental  situado en el Golfo de Guinea que se extiende de norte a sur en una estrecha franja de 120 kms de ancho entre Togo Y Nigeria y limita con Níger y Burkina Faso por el norte.",
+        "Lo habitan nueve millones de habitantes, franco-parlantes, pertenecientes a más de siete etnias que comparten por igual la belleza del país y la pobreza extrema, que lo coloca en los últimos puestos de desarrollo humano. IDH (índice desarrollo humano): puesto 172. La relación de médicos por habitante es de 0.1 cada 1000 (España 4.9), habiendo 0.2 camas por cada 1000 habitantes (España 3.2). La mortalidad infantil, la desnutrición y las enfermedades infecciosas son la primera causa de mortalidad."
+      ],
+      images: [
+        "https://ik.imagekit.io/300/0IyCDangboResources/FONDODANGBOOK_xcBh-s1kN.jpg?updatedAt=1637544532269",
+        "https://ik.imagekit.io/300/0IyCDangboResources/72571756_2120349764937330_6750359765085323264_n_4dW4DAjPp.jpg?updatedAt=1637545710094"
+      ]
+    }
+  ],
+  iyc: [
     "Fundada en febrero de 2014, fue inscrita en el Registro Nacional de Asociaciones del Ministerio del Interior en Mayo de 2014 y en el Registro de Organizaciones No Gubernamentales de Desarrollo con el Nº 2833 el 22 de enero de 2016. La Asociación Infancia y Cirugía en Dangbo está integrada por un grupo de Cirujanos, Anestesistas, Ginecólogos, Pediatras y Médicos de Familia y Enfermeras de Quirófano, Estudiantes de Medicina de 6ª Curso, Socios y Voluntarios con el objetivo de operar pacientes en el Hospital de Albergue del Amor Redentor en la localidad de Dangbo, región de Ouemé, en la República de Benín.",
-  text4:
-    "Nuestra intención es complementar la actividad de los Sanitarios locales, muy escasos en número ante la gran demanda de cirugía que existe en el País",
-  text5: ["NIF: G87030359", "Inscripción Registro: GRUPO1, SECCIÓN 1ª", "Número nacional 605499, el 27/05/2014"],
-  text6: [
-    "Domiclio:Carlos Picabea 1 28250 Torrelodones Madrid",
-    "Telf.: 646119698",
-    "asociacioncirugiaendangbo@gmail.com"
+    "Nuestra intención es complementar la actividad de los Sanitarios locales, muy escasos en número ante la gran demanda de cirugía que existe en el País"
   ],
-  text7: [
-    "Presidente: Dr. Jorge Parise Methol",
-    "Vicepresidente: Padre Martín Chognika",
-    "Secretaria: Dra. Itziar Tranche Álvarez-Cagigas",
-    "Tesorero: Dr. Carlos Ortiz Johansson"
-  ],
-  imageURL1: "https://ik.imagekit.io/300/0IyCDangboResources/FONDODANGBOOK_xcBh-s1kN.jpg?updatedAt=1637544532269",
-  imageURL2:
-    "https://ik.imagekit.io/300/0IyCDangboResources/72571756_2120349764937330_6750359765085323264_n_4dW4DAjPp.jpg?updatedAt=1637545710094"
+  association: {
+    registration: [
+      "NIF: G87030359",
+      "Inscripción Registro: GRUPO1, SECCIÓN 1ª",
+      "Número nacional 605499, el 27/05/2014"
+    ],
+    location: "Carlos Picabea 1 28250 Torrelodones Madrid",
+    phone: "+34 646 11 96 98",
+    email: "asociacioncirugiaendangbo@gmail.com",
+    team: [
+      "Presidente: Dr. Jorge Parise Methol",
+      "Vicepresidente: Padre Martín Chognika",
+      "Secretaria: Dra. Itziar Tranche Álvarez-Cagigas",
+      "Tesorero: Dr. Carlos Ortiz Johansson"
+    ]
+  }
 };
