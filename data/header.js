4f871f7f5f0b1075aa2bf4fba8fb5f0f9eaba00f
pmascaraque <ssm.pablo@gmail.com>
2021-12-13 17:35:47 +0100

    header changed news and about order

--- data/header.js
+++ data/header.js
@@ -4,11 +4,11 @@ export default {
     "https://camo.githubusercontent.com/511d7648a4b30a111c026ab63c93a5a2097b5f936119ec91e061d997fd511bff/68747470733a2f2f696e66616e6369617963697275676961656e64616e67626f2e6f72672f77702d636f6e74656e742f75706c6f6164732f323031352f31302f64616e67626f5f6c6f676f2e706e67",
   buttons: [
     { text: "Inicio", url: "/" },
-    { text: "Noticias", url: "/news" },
     {
       text: "Quiénes somos",
       url: "/about"
     },
+    { text: "Noticias", url: "/news" },
     { text: "Colabora", url: "/donate" }
   ]
 };
