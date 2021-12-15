1f6db0c59ca500488e49ecb4c7fc6a5951b1cbf5
Manuel Alférez <manuelalferezruiz@gmail.com>
2021-12-15 12:50:10 +0100

    Thank you page

--- pages/thanks.js
+++ pages/thanks.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Layout from "/components/layout/Layout";
-import data from "/data/thanks";
 import Confetti from "react-confetti";
 import Link from "next/link";
 import { useWindowSize } from "@react-hook/window-size";
+import useTranslation from "next-translate/useTranslation";
 
 function Thanks() {
+  const { t } = useTranslation();
   const [width, height] = useWindowSize();
 
   return (
     <Layout>
       <div className="h-screen w-full relative font-medium top-16 border-t-4 border-black">
         <div className="flex flex-col items-center md:mt-52 z-10 relative text-white md:h-screen">
-          <h1 className="mt-6 text-4xl md:text-6xl">{data.title}</h1>
-          <p className="text-center	mt-4 text-xl md:text-2xl px-2">{data.text}</p>
+          <h1 className="mt-6 text-4xl md:text-6xl">{t("thanks:title")}</h1>
+          <p className="text-center	mt-4 text-xl md:text-2xl px-2">{t("thanks:text")}</p>
           <Link href="/">
-            <a className="back-button z-10 relative mt-10">Volver</a>
+            <a className="back-button z-10 relative mt-10">{t("thanks:backButton")}</a>
           </Link>
         </div>
         <div className="absolute top-0 z-30">
@@ -29,7 +30,7 @@ function Thanks() {
           />
         </div>
         <img
-          src={data.imageURL}
+          src="https://ik.imagekit.io/300/0IyCDangboResources/BANNER1_RqeUUdexv.jpg?updatedAt=1634987720062"
           className="h-screen absolute z-0 top-0 overflow-y-hidden object-cover w-screen object-center"
         />
       </div>
