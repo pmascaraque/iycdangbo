45b63b54aa56f46127821646f3cd78e1df17078f
pmascaraque <ssm.pablo@gmail.com>
2022-01-09 20:24:24 +0100

    fixed return url on donate

--- components/donate/CheckoutForm.jsx
+++ components/donate/CheckoutForm.jsx
@@ -66,7 +66,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/thanks"
+        return_url: "https://infanciaycirugiaendangbo.org/thanks"
       }
     });
 
